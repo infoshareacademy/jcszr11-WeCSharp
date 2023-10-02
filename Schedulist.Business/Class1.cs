c8728b5115ad277999468b6ba7ea82a4e0615788
Marta Dębowska <debowska.marta@outlook.com>
2023-10-02 22:42:11 +0200

    Created initial folder structure and main solution

---
+++ Schedulist.Business/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Schedulist.Business
+{
+    public class Class1
+    {
+        //tu będą Metody
+        //Work mode service - metoda
+        //AddWorkMode
+        //EditWorkMode
+        //LogIn 
+        //logika biznesowa, tu hjest walidacja z potencjalnymi błędami
+    }
+}
---
+++ Schedulist/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Schedulist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. This is Schedulist!");
+        }
+    }
+}
