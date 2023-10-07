60721aebcf30d270edfb33fbe7817dc67775e2f4
Tomasz Gancarczyk <tom.g@onet.pl>
2023-10-07 10:35:58 +0200

    new file:   Schedulist.Business/Login.cs     added UserLogin() method to Program.cs

--- Schedulist/Program.cs
+++ Schedulist/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Schedulist
+﻿using Schedulist.Models;
+using Schedulist.Business;
+
+namespace Schedulist
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello. This is Schedulist!");
+            new Login().UserLogin();    //Metoda do logowania użytkownika
         }
     }
 }
