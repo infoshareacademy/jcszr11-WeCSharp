191b1d3a1b4adfad4c3a5dee9b1d117c3da5e4a0
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-10-08 13:43:18 +0200

    Work in progress.

---
+++ Schedulist.Models/CalendarEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedulist.Models
+{
+    internal class CalendarEvent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public int UserId { get; set; }
+    }
+}
