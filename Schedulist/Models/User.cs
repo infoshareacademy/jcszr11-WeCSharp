8a3dc9bf7d8dd0de731eba808e02ecac1e2d997a
Tomek <t.osmanowski@gmail.com>
2023-10-08 12:32:33 +0200

    connection of user privillages with displayed menu - proof of concept

--- Schedulist/Models/User.cs
+++ Schedulist/Models/User.cs
@@ -7,8 +7,33 @@ using System.Threading.Tasks;
 
 namespace Schedulist.Models
 {
+    enum Rights
+    {
+        IsAdmin = 10,
+        DeleteUser = 20,
+        AddUser = 30,
+        ReadOnly = 40
+    }
     internal class User
     {
+       
+        public List<Rights> Uprawnienia { get; set; }
+
+        public User(string name, params Rights[] rights)
+        {
+            this.Name= name;
+            if (rights.Length > 0) {
+                Uprawnienia = rights.ToList();
+            }
+
+            else 
+            {
+                Uprawnienia = new List<Rights> {Rights.ReadOnly };
+            }
+
+        }
+
+
         public int Id { get; private set; }
         public string Name { get; private set; }
         public string Surname { get; private set; }
--- Schedulist/Program.cs
+++ Schedulist/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Schedulist
+﻿using Schedulist.Models;
+
+namespace Schedulist
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello. This is Schedulist!");
+           List<User> users = new List<User>();
+            users.Add(new User("Basic user"));
+            users.Add(new User("Admin user", Rights.IsAdmin, Rights.DeleteUser, Rights.AddUser));
+
+            foreach(var user in users)
+            {
+                Console.WriteLine(user.Name);
+                if (user.Uprawnienia.Contains(Rights.DeleteUser)) { Console.WriteLine("usuwanie użytkownia menu"); }
+                if (user.Uprawnienia.Contains(Rights.AddUser)) { Console.WriteLine("dodawanie użytkownia menu"); }
+                if (user.Uprawnienia.Contains(Rights.IsAdmin)) { Console.WriteLine("admin menu"); }
+                if (user.Uprawnienia.Contains(Rights.ReadOnly)) { Console.WriteLine("readolny menu"); }
+
+            }    
+            
+            Console.ReadLine();
+
         }
     }
 }
