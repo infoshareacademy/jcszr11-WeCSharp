156a9f5ac990bfeff895ca94ca972790a3b76d4b
Marta Dębowska <debowska.marta@outlook.com>
2023-10-28 20:32:58 +0200

    Added new CalendarEvent record and worked initially on interfaces

---
+++ Schedulist.DAL/CalendarEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedulist.DAL
+{
+    public class CalendarEvent
+    {
+        public int CalendarEventId { get; set; }
+        public string CalendarEventName { get; set; }
+        public string CalendarEventDescription { get; set; }
+        public DateTime CalendarEventStartDateTime { get; set; }
+        public DateTime CalendarEventEndDateTime { get; set; }
+        public CalendarEvent(int calendarEventId,  string calendarEventName, string calendarEventDescription, DateTime calendarEventStartDateTime, DateTime calendarEventEndDateTime)
+        {
+            CalendarEventId = calendarEventId;
+            CalendarEventName = calendarEventName;
+            CalendarEventDescription = calendarEventDescription;
+            CalendarEventStartDateTime = calendarEventStartDateTime;
+            CalendarEventEndDateTime = calendarEventEndDateTime;
+        }
+
+    }
+}
---
+++ Schedulist.DAL/CsvCalendarEventRepository.cs
@@ -0,0 +1,46 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedulist.DAL
+{
+    public class CsvCalendarEventRepository : IRepository<CalendarEvent>
+    {
+        private string _pathToCsvFile;
+        private List<CalendarEvent> _calendars;
+        public CsvCalendarEventRepository(string pathToCsvFile)
+        {
+            _pathToCsvFile = pathToCsvFile;
+        }
+
+        public List<CalendarEvent> GetAll()
+        {
+            var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                //Delimiter = "\t",
+            };
+            using (var reader = new StreamReader(_pathToCsvFile))
+            using (var csv = new CsvReader(reader, csvConfig))
+            {
+                _calendars = csv.GetRecords<CalendarEvent>().ToList();
+                return _calendars;
+            }
+        }
+
+        public CalendarEvent GetById(int id)
+        {
+            return _calendars.Single(c => c.CalendarEventId == id);
+        }
+
+        public void Add(CalendarEvent item)
+        {
+            _calendars.Add(item);
+        }
+    }
+}
