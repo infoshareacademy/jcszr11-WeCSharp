49b04419d60356418ae561a64a3ef920d98f90a9
Jakub Organek <j.organek.jo@gmail.com>
2023-10-29 12:15:30 +0100

    WorkMOde Create

---
+++ Schedulist.DAL/WorkModes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedulist.DAL
+{
+    internal class WorkModes
+    {
+        public int WorkModeID { get; set; }
+        public string WorkModeName { get; set; }
+        
+        public WorkModes(int idWM, string nameWM)
+        {
+            WorkModeID = idWM;
+            WorkModeName = nameWM;
+        }
+    }
+}
