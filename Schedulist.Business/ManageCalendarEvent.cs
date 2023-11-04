5e42bdef3d634ce9fd4a1b65b2d2395a84b3f9a8
Marta Dębowska <debowska.marta@outlook.com>
2023-11-04 21:27:00 +0100

    Testing solution (new calendar events)

--- Schedulist.Business/ManageCalendarEvent.cs
+++ Schedulist.Business/ManageCalendarEvent.cs
@@ -15,10 +15,13 @@ namespace Schedulist.Business
         // public User currentUser;
 
         private List<CalendarEvent> _calendarEvents =
-             new CsvCalendarEventRepository("..\\..\\..\\CalendarEvents.csv").GetAllCalendarEvents();
+            new CsvCalendarEventRepository("..\\..\\..\\CalendarEvents.csv").GetAllCalendarEvents();
+
         private List<User> _userlist = new CsvUserRepository("Users.csv").GetAllUsers();
 
-        private CsvCalendarEventRepository csvCalendarEventRepository = new CsvCalendarEventRepository("..\\..\\..\\CalendarEvents.csv");
+        private CsvCalendarEventRepository csvCalendarEventRepository =
+            new CsvCalendarEventRepository("..\\..\\..\\CalendarEvents.csv");
+
         public ManageCalendarEvent(ICalendarEventRepository calendarEventRepository)
         {
             _calendarEventRepository = calendarEventRepository;
@@ -35,13 +38,16 @@ namespace Schedulist.Business
             Console.WriteLine("ID \t| Calendar Event Name \t\t|Date");
             for (int i = 0; i < _calendarEvents.Count; i++)
             {
-                Console.WriteLine($"{i} \t{_calendarEvents[i].CalendarEventName} \t\t\t {_calendarEvents[i].CalendarEventDate}");
+                Console.WriteLine(
+                    $"{i} \t{_calendarEvents[i].CalendarEventName} \t\t\t {_calendarEvents[i].CalendarEventDate}");
             }
+
             return null;
         }
+
         public CalendarEvent ShowUserCalendarEvent()
         {
-            
+
             Console.WriteLine("List of Calendar Events");
             Console.WriteLine("Choose User ID for which you want to show Calendar Events");
             int specifiedUserId = int.Parse(Console.ReadLine());
@@ -60,54 +66,59 @@ namespace Schedulist.Business
             {
                 Console.WriteLine($"{eventName}");
             }
+
             //for (int i = 1; i <= userCalendarEvents.Count; i++)
             //{
             //    Console.WriteLine($"Name {calendarEvents[i].CalendarEventName} starting at {calendarEvents[i].CalendarEventStartTime} and ending at {calendarEvents[i].CalendarEventEndTime}");
             //}
             return null;
         }
+
         public void CreateCalendarEvent()
         {
-            //var finishCreatingTask = false;
+           // var finishCreatingTask = false;
 
             //while (!finishCreatingTask)
             //{
-            Console.WriteLine("\nYou are creating new Calendar Event, please provide following data:");
-            int calendarEventId = 1;
-            Console.WriteLine("Name of Calendar Event:");
-            string calendarEventName = Console.ReadLine();
-            Console.WriteLine("Description of the Calendar Event:");
-            string calendarEventDescription = Console.ReadLine();
-            Console.WriteLine("Date of Calendar Event using format DD/MM/YYYY");
-            string dateValue = Console.ReadLine();
-            DateOnly.TryParse(dateValue, out var calendarEventDate);
-            //{
-            //    Console.WriteLine("You entered a valid date: " + dateValue);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Invalid date format. Please enter a date in the format 'dd/MM/yyyy'.");
-            //}
-            Console.WriteLine("Start time of Calendar Event using format HH:MM");
-            string startTime = Console.ReadLine();
-            TimeOnly.TryParse(startTime, out var calendarEventStartTime);
-            Console.WriteLine("End time of Calendar Event using format HH:MM");
-            string endTime = Console.ReadLine();
-            TimeOnly.TryParse(endTime, out var calendarEventEndTime);
+                Console.WriteLine("\nYou are creating new Calendar Event, please provide following data:");
+                int calendarEventId = 1;
+                Console.WriteLine("Name of Calendar Event:");
+                string calendarEventName = Console.ReadLine();
+                Console.WriteLine("Description of the Calendar Event:");
+                string calendarEventDescription = Console.ReadLine();
+                Console.WriteLine("Date of Calendar Event using format DD/MM/YYYY");
+                string dateValue = Console.ReadLine();
+                DateOnly.TryParse(dateValue, out var calendarEventDate);
+                //{
+                //    Console.WriteLine("You entered a valid date: " + dateValue);
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Invalid date format. Please enter a date in the format 'dd/MM/yyyy'.");
+                //}
+                Console.WriteLine("Start time of Calendar Event using format HH:MM");
+                string startTime = Console.ReadLine();
+                TimeOnly.TryParse(startTime, out var calendarEventStartTime);
+                Console.WriteLine("End time of Calendar Event using format HH:MM");
+                string endTime = Console.ReadLine();
+                TimeOnly.TryParse(endTime, out var calendarEventEndTime);
 
-            //Console.WriteLine("You created new task as following:");
-            //    Console.WriteLine(
-            //        $"Calendar Event Name:           |{calendarEventName}    \n Calendar Event Description:    |{calendarEventDescription}    \nCalendar Event Date: | {calendarEventDate} \n Start Time:   | {calendarEventStartTime} \nEnd Time:   | {calendarEventEndTime}");
-            //Console.Write(
-            //    "Press 'x' and Enter to close the app, or press any other key and Enter to continue creating tasks: ");
-            //if (Console.ReadLine() == "x") finishCreatingTask = true;
-            //else if (Console.ReadLine() == "y") //read menu
-            //    Console.WriteLine("\n");
+                //Console.WriteLine("You created new task as following:");
+                //    Console.WriteLine(
+                //        $"Calendar Event Name:           |{calendarEventName}    \n Calendar Event Description:    |{calendarEventDescription}    \nCalendar Event Date: | {calendarEventDate} \n Start Time:   | {calendarEventStartTime} \nEnd Time:   | {calendarEventEndTime}");
+                //
 
-            CalendarEvent calendarEvent = new CalendarEvent(calendarEventId, calendarEventName,
-                calendarEventDescription, calendarEventDate, calendarEventStartTime, calendarEventEndTime,
-                CurrentUser.currentUser);
-            csvCalendarEventRepository.AddCalendarEvent(calendarEvent);
+                CalendarEvent calendarEvent = new CalendarEvent(calendarEventId, calendarEventName,
+                    calendarEventDescription, calendarEventDate, calendarEventStartTime, calendarEventEndTime,
+                    CurrentUser.currentUser);
+                csvCalendarEventRepository.AddCalendarEvent(calendarEvent);
+              
+                //Console.Write("Press 'x' and Enter to close the app, or press any other key and Enter to continue creating tasks: ");
+                //if (Console.ReadLine() == "x") 
+                //{finishCreatingTask = true;}
+                //else if (Console.ReadLine() == "y") //read menu
+                //    Console.WriteLine("\n");
+           // }
         }
     }
 }
