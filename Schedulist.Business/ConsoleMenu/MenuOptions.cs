4a7a3e21fa96de1e72ab56a8c1119af738ea1cb5
Jakub Organek <j.organek.jo@gmail.com>
2023-11-12 01:17:01 +0100

    probowalem dopisac i zmodyfikowac WorkMode, licze na podpowiedzi

--- Schedulist.Business/ConsoleMenu/MenuOptions.cs
+++ Schedulist.Business/ConsoleMenu/MenuOptions.cs
@@ -48,7 +48,7 @@ namespace Schedulist.Business
             {
                 var option = Console.ReadKey();
                 if (option.Key == ConsoleKey.D1) new ManageWorkMode().ChooseOptionsWorkMode();
-                else if (option.Key == ConsoleKey.D2) Console.WriteLine("*****modify work mode*****");
+                else if (option.Key == ConsoleKey.D2) new ManageWorkMode().ChangeOptionWorkMode(CurrentUser.currentUser.Id);
                 else if (option.Key == ConsoleKey.D3) Console.WriteLine("*****delete work mode*****");
                 else if (option.Key == ConsoleKey.D4) new ManageWorkMode().ShowAllWorkModes();
                 else if (option.Key == ConsoleKey.Backspace) MenuMain.Run();
