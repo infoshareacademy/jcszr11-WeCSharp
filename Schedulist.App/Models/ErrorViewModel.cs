9758717c354e4e2486e03f40c82bd2c5248eefc5
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-11-19 13:17:48 +0100

    MVC project added.

---
+++ Schedulist.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Schedulist.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
