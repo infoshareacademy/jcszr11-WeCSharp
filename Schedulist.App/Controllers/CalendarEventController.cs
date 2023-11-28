9811844ddcebd4b8ef65a7521e5d68f10067e2ab
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-11-28 20:56:59 +0100

    Initial layout.

---
+++ Schedulist.App/Controllers/CalendarEventController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Schedulist.App.Controllers
+{
+    public class CalendarEventController : Controller
+    {
+        // GET: CalendarEventController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CalendarEventController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CalendarEventController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CalendarEventController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CalendarEventController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CalendarEventController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CalendarEventController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CalendarEventController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
