5ced26acbfd76518750e971045461ab0c3234756
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-12-07 18:50:55 +0100

    Adding identity first stage completed.

--- Schedulist.DAL/DbContact.cs
+++ Schedulist.DAL/DbContact.cs
@@ -12,5 +12,14 @@ namespace Schedulist.DAL
     public class DBContact : IdentityDbContext<IdentityUser>
     {
         public DBContact(DbContextOptions<DBContact> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
     }
+
 }
