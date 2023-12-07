e818cf9012a1c5f657b9968c7233de8a7cced7af
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-12-07 21:22:20 +0100

    Deleteing unused folders.

--- Schedulist.App1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Schedulist.App1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
