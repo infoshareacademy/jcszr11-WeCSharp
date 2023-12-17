a32db03fb9a899b7ecdfbce62d4e9c2146042bd0
Marta Dębowska <debowska.marta@outlook.com>
2023-12-17 12:20:19 +0100

    Added Calendar Event Service

---
+++ Schedulist.App/Services/CalendarEventService.cs
@@ -0,0 +1,18 @@
+﻿using Schedulist.DAL;
+
+namespace Schedulist.App.Services
+{
+    public class CalendarEventService
+    {
+        private readonly CsvCalendarEventRepository repository;
+        public List<CalendarEvent> ShowUserCalendarEvent(User user, DateOnly specifiedDate)
+        {
+            var calendarEvents = repository.GetAllCalendarEvents();
+            var userCalendarEvents = calendarEvents
+                .Where(c => c.AssignedToUser == user.Id && c.CalendarEventDate == specifiedDate)
+                .OrderBy(c => c.CalendarEventStartTime)
+                .ToList();
+            return userCalendarEvents;
+        }
+    }
+}
