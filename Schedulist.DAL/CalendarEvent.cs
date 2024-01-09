6eb2f52da1cf122cee6589fb88b45b6944ec0fe1
Marta Dębowska <debowska.marta@outlook.com>
2024-01-09 21:32:07 +0100

    Added display Names

--- Schedulist.DAL/CalendarEvent.cs
+++ Schedulist.DAL/CalendarEvent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,22 @@ namespace Schedulist.DAL
     public class CalendarEvent
     {
         [Name("calendarEventId")]
+        [Display(Name = "Calendar Event ID")]
         public int CalendarEventId { get; set; }
         [Name("calendarEventName")]
+        [Display(Name = "Calendar Event Name")]
         public string CalendarEventName { get; set; }
         [Name("calendarEventDescription")]
+        [Display(Name = "Description")]
         public string CalendarEventDescription { get; set; }
         [Name("calendarEventDate")]
+        [Display(Name = "Date")]
         public DateOnly CalendarEventDate { get; set; }
         [Name("calendarEventStartTime")]
+        [Display(Name = "Start Time")]
         public TimeOnly CalendarEventStartTime { get; set; }
         [Name("calendarEventEndTime")]
+        [Display(Name = "End Time")]
         public TimeOnly CalendarEventEndTime { get; set; }
         [Name("userID")]
         public int AssignedToUser { get; set; } 
