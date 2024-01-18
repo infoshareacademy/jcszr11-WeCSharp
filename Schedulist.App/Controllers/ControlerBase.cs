eda0282ddb357a2318c6f10a4eb078d5eb5cd961
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2024-01-18 20:31:50 +0100

    Groundwork for pop-ups notifications.

--- Schedulist.App/Controllers/ControlerBase.cs
+++ Schedulist.App/Controllers/ControlerBase.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Schedulist.App.Models.Enum;
 
 namespace Schedulist.App.Controllers
 {
@@ -10,6 +12,17 @@ namespace Schedulist.App.Controllers
         {
             Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-GB");
             this.logger = logger;
+        }
+        public void Notify(string title, string message = "", NotificationType notificationType = NotificationType.success)
+        {
+            var msg = new
+            {
+                title = title,
+                message = message,
+                type = notificationType.ToString(),
+            };
+
+            TempData["Message"] = JsonConvert.SerializeObject(msg);
         }
     }
 }
--- Schedulist.App/wwwroot/js/site.js
+++ Schedulist.App/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function DeleteCalendarEvent(actionUrl) {
+    Swal.fire({
+        title: 'Confirm Deletion',
+        text: 'Are you sure you want to delete your calendar event?',
+        icon: 'question',
+        showCancelButton: true
+    }).then(result => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: actionUrl,
+                type: 'POST',
+                success: function (result) {
+                    localStorage.setItem('calendarEventDeletedMessage', 'Your calendar event has been deleted successfully');
+                    location.reload();
+                },
+                error: function (error) {
+                    alert("failure");
+                }
+            });
+
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            Swal.fire('Cancelled', 'operation has been canceled', 'info');
+        }
+    });
+}
