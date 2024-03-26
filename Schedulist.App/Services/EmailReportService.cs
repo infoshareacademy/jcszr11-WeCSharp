fb8074cffe50b0c0e295137bf8024a2992f3cc67
Tomasz Gancarczyk <tom.g@onet.pl>
2024-03-26 05:19:00 +0100

    added packages and service class for sending reports through email

---
+++ Schedulist.App/Services/EmailReportService.cs
@@ -0,0 +1,42 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System.Net.Mail;
+
+namespace Schedulist.App.Services
+{
+    public class EmailReportService
+    {
+        public static async Task SendEmailAsync(string emailAdress)
+        {
+            var email = new MimeMessage();
+
+            //TODO fill from / subject / body
+            email.From.Add(new MailboxAddress("name", "adress"));
+            email.To.Add(new MailboxAddress("Customer", emailAdress));
+            email.Subject = "";
+            email.Body = new TextPart("plain")
+            {
+                Text = "Witaj! \n" //+
+                    //"Dziękujemy za rezerwację samochodu w CodeDrivers. Poniżej przesyłamy szczegóły Twojej rezerwacji. \n" +
+                    //$"Auto: {reservation.Brand} {reservation.Model} \n" +
+                    //$"Data odbioru: {reservation.ReservationFrom.ToShortDateString()} \n" +
+                    //$"Data zwrotu: {reservation.ReservationTo.ToShortDateString()} \n" +
+                    //$"Cena całkowita: {reservation.TotalReservationPrice} zł \n" +
+                    //"W razie pytań jesteśmy do Twoje dyspozycji pod numerem 999 666 222 lub e-mailem codedrivers@o2.pl. \n" +
+                    //"Pozdrawiamy \n" +
+                    //"Ekipa CodeDrivers";
+        };
+
+        //using (var client = new SmtpClient())
+        //{
+        //    await client.ConnectAsync("smtp.poczta.onet.pl", 587, false); //???
+        //    await client.AuthenticateAsync("email", "password");
+        //    await client.SendAsync(emailMessage);
+
+        //    await client.DisconnectAsync(true);
+        //}
+
+
+    }
+}
+}
